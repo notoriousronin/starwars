05c84884f6455369f290595605f82516b90ee0de
notoriousronin <103944609+notoriousronin@users.noreply.github.com>
2022-10-07 17:39:43 +0000

    Deploying to gh-pages from @ notoriousronin/starwars@f8cba3f8bffe46e42b28f0f6bf804f2fb0d69873 🚀

---
+++ index.37cfb47f.js
@@ -0,0 +1,2 @@
+fetch("https://swapi.dev/api/people/4").then((function(n){return n.json()})).then(console.log).catch((function(n){return console.log(n)}));
+//# sourceMappingURL=index.37cfb47f.js.map
---
+++ index.7466bda0.js
@@ -0,0 +1,2 @@
+fetch("https://swapi.dev/api/people/4").then((e=>e.json())).then(console.log).catch((e=>console.log(e)));
+//# sourceMappingURL=index.7466bda0.js.map
